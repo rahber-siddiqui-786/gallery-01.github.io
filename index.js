c0d3fcab2c61d737ca57c3857cdbc97c45f56f6a
rahber-siddiqui-786 <88225962+rahber-siddiqui-786@users.noreply.github.com>
2022-01-02 19:03:29 +0530

    gallery

---
+++ index.js
@@ -0,0 +1,76 @@
+// alert('clicked');
+let image = document.getElementsByTagName('img');
+let div = document.getElementsByTagName('div');
+
+
+image[0].addEventListener('click',function(){
+    div[0].classList.toggle('full-size-div');
+    image[0].classList.toggle('full-size-img');
+    
+});
+
+image[1].addEventListener('click',function(){
+    div[1].classList.toggle('full-size-div');
+    image[1].classList.toggle('full-size-img');
+    
+});
+
+image[2].addEventListener('click',function(){
+    div[2].classList.toggle('full-size-div');
+    image[2].classList.toggle('full-size-img');
+    
+});
+
+image[3].addEventListener('click',function(){
+    div[3].classList.toggle('full-size-div');
+    image[3].classList.toggle('full-size-img');
+    
+});
+
+image[4].addEventListener('click',function(){
+    div[4].classList.toggle('full-size-div');
+    image[4].classList.toggle('full-size-img');
+    
+});
+
+image[5].addEventListener('click',function(){
+    div[5].classList.toggle('full-size-div');
+    image[5].classList.toggle('full-size-img');
+    
+});
+
+image[6].addEventListener('click',function(){
+    div[6].classList.toggle('full-size-div');
+    image[6].classList.toggle('full-size-img');
+    
+});
+
+image[7].addEventListener('click',function(){
+    div[7].classList.toggle('full-size-div');
+    image[7].classList.toggle('full-size-img');
+    
+});
+
+image[8].addEventListener('click',function(){
+    div[8].classList.toggle('full-size-div');
+    image[8].classList.toggle('full-size-img');
+    
+});
+
+image[9].addEventListener('click',function(){
+    div[9].classList.toggle('full-size-div');
+    image[9].classList.toggle('full-size-img');
+    
+});
+
+image[10].addEventListener('click',function(){
+    div[10].classList.toggle('full-size-div');
+    image[10].classList.toggle('full-size-img');
+    
+});
+
+image[11].addEventListener('click',function(){
+    div[11].classList.toggle('full-size-div');
+    image[11].classList.toggle('full-size-img');
+    
+});
